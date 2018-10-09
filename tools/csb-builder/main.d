02a27dbb7b8a758c6eea0306c4d0cc1d02491196
Leandro Zanol <lzanol@gmail.com>
2018-10-09 00:20:11 -0700

    update sb builder to comply new compiler

--- tools/csb-builder/main.d
+++ tools/csb-builder/main.d
@@ -1,4 +1,5 @@
 import std.stdio:writeln;
+import std.path:baseName;
 import std.file;
 import std.conv;
 import std.string;
@@ -6,18 +7,18 @@ import std.math;
 import std.algorithm;
 import std.regex;
 
-void wavesToDrm(string path, string outputPath)
-{
-	union SampleData
-	{
+void wavesToRaw(string path, string outputPath) {
+	union SampleData {
 		int* channelData;
 		short* dataSet;
 	}
 
-    uint noteDur = 44100*3; // (...) 3 segs (samples).
+	const uint SAMPLING_RATE = 48000;
+
+    uint noteDur = SAMPLING_RATE*4; // secs in samples
 	string numPartPattern = r"\d+(?=.*\.\w+$)";
-	int totalFiles;
-	int[] drm;
+	ulong totalFiles;
+	int[] raw;
 	int[] smps;
 	void[] smpsB;
 	SampleData smpData;
@@ -25,95 +26,93 @@ void wavesToDrm(string path, string outputPath)
 	float fade0 = 0.75f;
 	float fade1 = 1f;
 
-	// Percorre os subdiretórios da pasta de samples.
-	foreach (DirEntry entry; dirEntries(path, SpanMode.shallow))
-	{
-		if (entry.isDir() && std.path.baseName(entry.name) == "GrandPiano")
-		{
-			string[] audioNames;
+	// look for subdirs
+	foreach (DirEntry entry; dirEntries(path, SpanMode.shallow)) {
+		if (!entry.isDir())
+			continue;
 
-			// Percorre os samples (arquivos) em cada pasta.
-			foreach (DirEntry audioEntry; dirEntries(entry.name, "*.wav", SpanMode.shallow))
-				version (Windows)
-				{
-					// Se não for um arquivo oculto.
-					if (!(audioEntry.attributes & 2))
-						audioNames ~= audioEntry.name;
-				}
-				else audioNames ~= audioEntry.name;
+		string[] audioNames;
 
-			// Ordena os arquivos por número.
-			sort!(delegate bool(string a, string b)
+		// reads each sample of current subdir
+		foreach (DirEntry audioEntry; dirEntries(entry.name, "*.wav", SpanMode.shallow)) {
+			version (Windows) {
+				// ignores hidden files (Windows)
+				if (!(audioEntry.attributes & 2))
+					audioNames ~= audioEntry.name;
+			} else audioNames ~= audioEntry.name;
+		}
+
+		sort!()(audioNames);
+
+		writeln("Concatenating...");
+		writeln(audioNames);
+
+		totalFiles = audioNames.length;
+		raw = new int[totalFiles*noteDur];
+
+		for (int j = 0; j < totalFiles; ++j) {
+			// reading wave (skiping header)
+			smpsB = read(audioNames[j])[44..$];
+			// normalizing array size
+			smpsB ~= new void[smpsB.length%4];
+			smps = cast(int[])smpsB;
+
+			// fills with silence note duration is less
+			if (smps.length < noteDur)
+				smps ~= (new int[noteDur - smps.length])[] = 0;
+
+			ulong totalSamples = smps.length;
+
+			// Amplitude
+			for (int k = 0; k < totalSamples; ++k)
 			{
-				return to!int(match(a, numPartPattern).hit) < to!int(match(b, numPartPattern).hit);
-			})(audioNames);
-
-			totalFiles = audioNames.length;
-			drm = new int[totalFiles*noteDur];
-
-			for (int j = 0; j < totalFiles; ++j)
-			{
-				// Lê o wave sem o cabeçalho.
-				smpsB = read(audioNames[j])[44..$];
-				smpsB ~= new void[smpsB.length%4];
-				smps = cast(int[])smpsB;
-
-				if (smps.length < noteDur)
-					smps ~= (new int[noteDur - smps.length])[] = 0;
-
-				int totalSamples = smps.length;
-
-				// Amplitude
-				for (int k = 0; k < totalSamples; ++k)
-				{
-					smpData.channelData = &smps[k];
-					*smpData.dataSet *= amp;
-					*(smpData.dataSet + 1) *= amp;
-				}
-
-				int fadePos0 = cast(int)(totalSamples*fade0);
-				int fadePos1 = cast(int)(totalSamples*fade1);
-				double totalFadeSamples = cast(double)(fadePos1 - fadePos0);
-				double mult;
-
-				// Fade-out
-				for (int k = fadePos0; k < totalSamples; ++k)
-				{
-					if (k < fadePos1)
-						mult = 1f - (k - fadePos0)/totalFadeSamples;
-					else mult = 0;
-
-					smpData.channelData = &smps[k];
-					*smpData.dataSet *= mult;
-					*(smpData.dataSet + 1) *= mult;
-				}
-
-				// (...) Remove a assinatura da Sony (Sound Forge).
-				//smps[$ - 44..$] = 0;
-
-				// Corta o sample se exceder ou preenche com silêncio se faltar.
-				drm[j*noteDur..(j + 1)*noteDur] = smps[0..noteDur];
-
-				delete smps;
+				smpData.channelData = &smps[k];
+				*smpData.dataSet *= amp;
+				*(smpData.dataSet + 1) *= amp;
 			}
 
-			write(text(outputPath, "/", std.path.baseName(entry.name), ".drm"), drm);
+			int fadePos0 = cast(int)(totalSamples*fade0);
+			int fadePos1 = cast(int)(totalSamples*fade1);
+			double totalFadeSamples = cast(double)(fadePos1 - fadePos0);
+			double mult;
 
-			delete drm;
-			delete audioNames;
+			// Fade-out
+			for (int k = fadePos0; k < totalSamples; ++k)
+			{
+				if (k < fadePos1)
+					mult = 1f - (k - fadePos0)/totalFadeSamples;
+				else mult = 0;
+
+				smpData.channelData = &smps[k];
+				*smpData.dataSet *= mult;
+				*(smpData.dataSet + 1) *= mult;
+			}
+
+			// (...) Remove a assinatura da Sony (Sound Forge).
+			//smps[$ - 44..$] = 0;
+
+			// Corta o sample se exceder ou preenche com silêncio se faltar.
+			raw[j*noteDur..(j + 1)*noteDur] = smps[0..noteDur];
+
+			smps.destroy();
 		}
+
+		write(text(outputPath, "/", baseName(entry.name), ".csb"), raw);
+
+		raw.destroy();
+		audioNames.destroy();
 	}
 }
 
-void drmToWave(string path)
+void rawToWave(string path)
 {
 	const uint SAMPLING_RATE = 44100;
 	const uint NUM_CHANNELS = 2;
 	const uint BITS_SAMPLE = 16;
 
 	ubyte[] h;
-	int[] drm = cast(int[])read(path);
-	uint tData = drm.length*4;
+	auto raw = cast(ubyte[])read(path);
+	const tData = raw.length;
 
 	h ~= "RIFF"; // ChunkID
 	h ~= intToLEBytes(tData + 36, 4); // ChunkSize
@@ -129,13 +128,13 @@ void drmToWave(string path)
 	h ~= "data"; // Subchunk2ID
 	h ~= intToLEBytes(tData, 4); // Subchunk2Size
 
-	write(path[0..$ - 3] ~ "wav", h ~ cast(ubyte[])drm);
+	write(path[0..$ - 3] ~ "wav", h ~ raw);
 
-	delete h;
-	delete drm;
+	h.destroy();
+	raw.destroy();
 }
 
-ubyte[] intToLEBytes(uint n, uint size)
+ubyte[] intToLEBytes(ulong n, uint size)
 {
 	ubyte[] nb;
 
@@ -161,8 +160,8 @@ ubyte[] intToLEBytes(uint n, uint size)
 
 int main(char[][] args)
 {
-	wavesToDrm("audio", "output");
-	//drmToWave("output/Mixed.drm");
+	wavesToRaw("input", "output");
+	//rawToWave("output/Mixed.drm");
 
     return 0;
 }
